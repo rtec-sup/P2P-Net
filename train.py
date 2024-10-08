767b19b76b00e1a2f32d8a4b216a5f606bd003fb
phuc.thai <phuc.thai@rikai.technology>
2024-10-08 10:51:16 +0700

    Custom dataset

--- train.py
+++ train.py
@@ -11,54 +11,49 @@ import torch.nn.functional as F
 from dataset import *
 from utils import *
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '0, 1, 2, 3'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
 
 def train():
     parser = argparse.ArgumentParser('FGVC', add_help=False)
-    parser.add_argument('--epochs', type=int, default=300, help="training epochs")
-    parser.add_argument('--batch_size', type=int, default=16, help="batch size for training")
-    parser.add_argument('--resume', type=str, default="", help="resume from saved model path")
-    parser.add_argument('--dataset_name', type=str, default="cub", help="dataset name")
+    parser.add_argument('--epochs', type=int, default=300,
+                        help="training epochs")
+    parser.add_argument('--batch_size', type=int, default=16,
+                        help="batch size for training")
+    parser.add_argument('--resume', type=str, default="",
+                        help="resume from saved model path")
+    parser.add_argument('--dataset_name', type=str,
+                        default="custom", help="dataset name")
     parser.add_argument('--topn', type=int, default=4, help="parts number")
-    parser.add_argument('--backbone', type=str, default="resnet50", help="backbone")
+    parser.add_argument('--backbone', type=str,
+                        default="resnet50", help="backbone")
     parser.add_argument('--lr', type=float, default=2e-3, help="learning rate")
     args, _ = parser.parse_known_args()
     epochs = args.epochs
     batch_size = args.batch_size
 
-    ## Data
-    data_config = { "air": [100, "../Data/fgvc-aircraft-2013b"], 
-                    "car": [196, "../Data/stanford_cars"], 
-                    "dog": [120, "../Data/StanfordDogs"],
-                    "cub": [200, "../Data/CUB_200_2011"], 
-                    }
+    # Data
+    data_config = {"custom": [87, "./"]}
     dataset_name = args.dataset_name
     classes_num, data_root = data_config[dataset_name]
-    if dataset_name == 'air':
-        trainset = AIR(root=data_root, is_train=True, data_len=None)
-        testset = AIR(root=data_root, is_train=False, data_len=None)
-    elif dataset_name == 'car':
-        trainset = CAR(root=data_root, is_train=True, data_len=None)
-        testset= CAR(root=data_root, is_train=False, data_len=None)
-    elif dataset_name == 'dog':
-        trainset = DOG(root=data_root, is_train=True, data_len=None)
-        testset = DOG(root=data_root, is_train=False, data_len=None)
-    elif dataset_name == 'cub':
-        trainset = CUB(root=data_root, is_train=True, data_len=None)
-        testset = CUB(root=data_root, is_train=False, data_len=None)
-    num_workers = 16 if torch.cuda.is_available() else 0
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers,drop_last=False)
+    trainset = CustomDataset(root=data_root, is_train=True, data_len=None)
+    testset = CustomDataset(root=data_root, is_train=False, data_len=None)
 
-    ## Output
+    num_workers = 16 if torch.cuda.is_available() else 0
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers, drop_last=False)
+
+    # Output
     topn = args.topn
     exp_dir = dataset_name + '_' + args.backbone + '_' + str(topn)
     os.makedirs(exp_dir, exist_ok=True)
 
-    ## Model
+    # Model
     if args.resume != "":
         net = torch.load(args.resume)
     else:
-        net = load_model(backbone=args.backbone, pretrain=True, require_grad=True, classes_num=classes_num, topn=topn)
+        net = load_model(backbone=args.backbone, pretrain=True,
+                         require_grad=True, classes_num=classes_num, topn=topn)
     if torch.cuda.is_available():
         device = torch.device('cuda')
         net = net.to(device)
@@ -66,22 +61,24 @@ def train():
     else:
         device = torch.device('cpu')
         netp = net
-    
 
-    ## Train
+    # Train
     CELoss = nn.CrossEntropyLoss()
-    deep_paras = [para for name,para in net.named_parameters() if "backbone" not in name]
+    deep_paras = [para for name, para in net.named_parameters()
+                  if "backbone" not in name]
     optimizer = optim.SGD(
-        [{'params':deep_paras}, 
-        {'params': net.backbone.parameters(), 'lr': args.lr/10.0}],
+        [{'params': deep_paras},
+         {'params': net.backbone.parameters(), 'lr': args.lr/10.0}],
         lr=args.lr, momentum=0.9, weight_decay=5e-4)
 
     max_val_acc = 0
     for epoch in range(1, epochs+1):
         print('\nEpoch: %d' % epoch)
         # update learning rate
-        optimizer.param_groups[0]['lr'] = cosine_anneal_schedule(epoch, epochs, args.lr)
-        optimizer.param_groups[1]['lr'] = cosine_anneal_schedule(epoch, epochs, args.lr/10.0)
+        optimizer.param_groups[0]['lr'] = cosine_anneal_schedule(
+            epoch, epochs, args.lr)
+        optimizer.param_groups[1]['lr'] = cosine_anneal_schedule(
+            epoch, epochs, args.lr/10.0)
 
         net.train()
         num_correct = [0] * 4
@@ -93,8 +90,9 @@ def train():
 
             # forward
             optimizer.zero_grad()
-            y1, y2, y3, y4, yp1, yp2, yp3, yp4, part_probs, f1_m, f1, f2_m, f2, f3_m, f3 = netp(inputs)
-            
+            y1, y2, y3, y4, yp1, yp2, yp3, yp4, part_probs, f1_m, f1, f2_m, f2, f3_m, f3 = netp(
+                inputs)
+
             loss1 = smooth_CE(y1, targets, 0.7) * 1
             loss2 = smooth_CE(y2, targets, 0.8) * 1
             loss3 = smooth_CE(y3, targets, 0.9) * 1
@@ -106,7 +104,8 @@ def train():
             lossp2 = smooth_CE(yp2, targets_parts, 0.8)
             lossp3 = smooth_CE(yp3, targets_parts, 0.9)
             lossp4 = smooth_CE(yp4, targets_parts, 1)
-            lossp_rank = ranking_loss(part_probs, list_loss(yp4, targets_parts).view(batch_size, topn)) # higher prob, smaller loss
+            lossp_rank = ranking_loss(part_probs, list_loss(yp4, targets_parts).view(
+                batch_size, topn))  # higher prob, smaller loss
             loss_parts = lossp1 + lossp2 + lossp3 + lossp4 + lossp_rank
 
             p, q = F.log_softmax(f1_m, dim=-1), F.softmax(f1, dim=-1)
@@ -115,9 +114,9 @@ def train():
             loss_reg += torch.mean(-torch.sum(p*q, dim=-1)) * 0.1
             p, q = F.log_softmax(f3_m, dim=-1), F.softmax(f3, dim=-1)
             loss_reg += torch.mean(-torch.sum(p*q, dim=-1)) * 0.1
-            
+
             loss = loss_img + loss_parts + loss_reg
-            
+
             _, p1 = torch.max(y1.data, 1)
             _, p2 = torch.max(y2.data, 1)
             _, p3 = torch.max(y3.data, 1)
@@ -127,20 +126,20 @@ def train():
             num_correct[1] += p2.eq(targets.data).cpu().sum()
             num_correct[2] += p3.eq(targets.data).cpu().sum()
             num_correct[3] += p4.eq(targets.data).cpu().sum()
-            
+
             # backward
             loss.backward()
             optimizer.step()
-        
-        
-        ## result
+
+        # result
         total = len(trainset)
         acc1 = 100. * float(num_correct[0]) / total
         acc2 = 100. * float(num_correct[1]) / total
         acc3 = 100. * float(num_correct[2]) / total
         acc4 = 100. * float(num_correct[3]) / total
-        
-        result_str = 'Iteration %d (train) | acc1 = %.5f | acc2 = %.5f | acc3 = %.5f | acc4 = %.5f \n' % (epoch, acc1, acc2, acc3, acc4)
+
+        result_str = 'Iteration %d (train) | acc1 = %.5f | acc2 = %.5f | acc3 = %.5f | acc4 = %.5f \n' % (
+            epoch, acc1, acc2, acc3, acc4)
         print(result_str)
         with open(exp_dir + '/results_train.txt', 'a') as file:
             file.write(result_str)
@@ -152,8 +151,9 @@ def train():
                 net.cpu()
                 torch.save(net.state_dict(), './' + exp_dir + '/model.pth')
                 net.to(device)
-            
-            result_str = 'Iteration %d | acc1 = %.5f | acc2 = %.5f | acc3 = %.5f | acc4 = %.5f | acc_test = %.5f \n' % (epoch, acc1, acc2, acc3, acc4, acc_test)
+
+            result_str = 'Iteration %d | acc1 = %.5f | acc2 = %.5f | acc3 = %.5f | acc4 = %.5f | acc_test = %.5f \n' % (
+                epoch, acc1, acc2, acc3, acc4, acc_test)
             print(result_str)
             with open(exp_dir + '/results_test.txt', 'a') as file:
                 file.write(result_str)
